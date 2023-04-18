0ad60e79f541bce35a851bd52d6781b0fdf15b25
MaxChernoshey <89794858+MaxChernoshey@users.noreply.github.com>
2023-04-18 23:41:47 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,8 @@ node default {
  package { 'mysql':
   ensure => installed,
   }
+  notify {"Running os ${facts['os']['name']}
+version ${facts['os']['release']['full']}":}
 }
 node slave1.puppet {
   package { 'htop':
