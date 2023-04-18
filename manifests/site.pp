d8412a41e6d509214b416332f9cdf28080055698
MaxChernoshey <89794858+MaxChernoshey@users.noreply.github.com>
2023-04-18 23:52:53 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,4 +9,8 @@ node slave1.puppet {
   package { 'htop':
   ensure => present,
   }
+  file {'/tmp/osinfo.txt':
+    ensure => file,
+    content => "Running os ${facts['os']['name']} version ${facts['os']['release']['full']}",
+  }
 }
