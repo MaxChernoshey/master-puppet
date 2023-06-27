51b9e068f6e40e803492cc01a178e76bd28d4488
MaxChernoshey <89794858+MaxChernoshey@users.noreply.github.com>
2023-06-27 23:08:39 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,28 @@
-node default {
- package { 'mysql':
-  ensure => installed,
-  }
-  notify {"Running os ${facts['os']['name']}
-version ${facts['os']['release']['full']}":}
-}
 node slave1.puppet {
-  package { 'htop':
-  ensure => present,
+  package { 'apache2':
+    ensure => installed,
   }
-  file {'/tmp/osinfo.txt':
-    ensure => file,
-    content => "Running os ${facts['os']['name']} version ${facts['os']['release']['full']}",
+  file { '/var/www/html/index.html':
+    ensure  => absent,
+  }
+  file { '/var/www/html/index.html': 
+    path => '/var/www/html/index.html',
+    source => 'https://raw.githubusercontent.com/MaxChernoshey/itacademy-devops-files/master/02-tools/index.html',
   }
 }
+
+node slave2.puppet {
+  package { 'php':
+    ensure => installed,
+  
+  package { 'apache2':
+    ensure => installed,
+  }
+  file { '/var/www/html/index.html':
+    ensure  => absent,
+  }
+  file { '/var/www/html/index.php': 
+    path => '/var/www/html/index.php',
+    source => 'https://raw.githubusercontent.com/MaxChernoshey/itacademy-devops-files/master/02-tools/index.php',
+  }
+ }
